5d1ed024a7b0810d711ec0af40c33f47c08ebfd3
Scot Forbes <scforbes@expedia.com>
2017-04-08 19:03:10 -0700

    Fix XML configuration.

--- src/Wave.Core/Configuration/ConfigurationSection.cs
+++ src/Wave.Core/Configuration/ConfigurationSection.cs
@@ -201,12 +201,14 @@ namespace Wave.Configuration
                 set { base["messageRetryLimit"] = value; }
             }
 
+            [ConfigurationProperty("prefetchCount", IsRequired = false)]
             public int PrefetchCount
             {
                 get { return (int)base["prefetchCount"]; }
                 set { base["prefetchCount"] = value; }
             }
 
+            [ConfigurationProperty("delayPrefetchCount", IsRequired = false)]
             public int DelayPrefetchCount
             {
                 get { return (int)base["delayPrefetchCount"]; }
