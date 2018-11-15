d120ae8038bb3f777040cf5e0fa7e0cdbe792514
Trey Gorman <jgorman@expedia.com>
2018-11-15 15:40:14 -0800

    Convert remaining projects used by EQS to VS 2017 format.

--- src/Wave.ServiceHosting.TopShelf/Properties/AssemblyInfo.cs
+++ src/Wave.ServiceHosting.TopShelf/Properties/AssemblyInfo.cs
@@ -12,40 +12,10 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 */
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Wave.ServiceHosting.TopShelf")]
-[assembly: AssemblyDescription("Allows Wave Service Bus applications to be hosted as TopShelf services.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jonathan Holland")]
-[assembly: AssemblyProduct("Wave.ServiceHosting.TopShelf")]
-[assembly: AssemblyCopyright("Copyright © 2014 Jonathan Holland")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0bc850ce-6619-4250-a94a-221fce74a0a4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
