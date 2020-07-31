4a5fe4782d111244c3c921f6049a5881a90c09d9
J.D. Satlin <jsatlin@expedia.com>
2020-07-31 16:54:38 -0700

    Update main src folder libraries to new csproj format

--- src/Wave.Serialization.Xml/Properties/AssemblyInfo.cs
+++ src/Wave.Serialization.Xml/Properties/AssemblyInfo.cs
@@ -16,18 +16,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Wave.Serialization.Xml")]
-[assembly: AssemblyDescription("Enables XML serialization support for Wave Service Bus.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jonathan Holland")]
-[assembly: AssemblyProduct("Wave.Serialization.Xml")]
-[assembly: AssemblyCopyright("Copyright © 2014 Jonathan Holland")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -35,17 +23,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7a0ecfe6-0c8a-4e1e-bb93-d8edceb44040")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
